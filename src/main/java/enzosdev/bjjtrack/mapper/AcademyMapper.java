a91bf35af23f105102e075a51dbda6006b5f3056
enzosdev <enzohfs@gmail.com>
2026-02-15 20:55:35 -0300

    add map for Academy toEntity and AcademyCreateResponseDTO toResponse

--- src/main/java/enzosdev/bjjtrack/mapper/AcademyMapper.java
+++ src/main/java/enzosdev/bjjtrack/mapper/AcademyMapper.java
@@ -1,4 +1,36 @@
 package enzosdev.bjjtrack.mapper;
 
+import enzosdev.bjjtrack.dto.AcademyCreateDTO;
+import enzosdev.bjjtrack.dto.AcademyCreateResponseDTO;
+import enzosdev.bjjtrack.entity.Academy;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AcademyMapper {
+
+
+    public Academy toEntity(AcademyCreateDTO dto){
+        if (dto == null) {
+            return null;
+        }
+        Academy academy = new Academy();
+        academy.setName(dto.getName());
+        academy.setLogo_url(dto.getLogoUrl());
+        academy.setSlug(dto.getSlug());
+        return academy;
+    }
+
+
+    public AcademyCreateResponseDTO toResponse(Academy academy){
+        if (academy == null) {
+            return null;
+        }
+        AcademyCreateResponseDTO dto = new AcademyCreateResponseDTO();
+        dto.setId(academy.getId());
+        dto.setName(academy.getName());
+        dto.setLogoUrl(academy.getLogo_url());
+        dto.setSlug(academy.getSlug());
+        return dto;
+    }
+
 }
