cc4a05c1d8c0218d4268c46e81f7e1d5ef5efc9a
enzosdev <enzohfs@gmail.com>
2026-02-15 20:17:27 -0300

    fix: change signupResponse to signupRequest

--- src/main/java/enzosdev/bjjtrack/dto/SignupResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/SignupResponse.java
@@ -1,16 +1,4 @@
 package enzosdev.bjjtrack.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class SignupResponse {
-
-    private AcademyCreateDTO academy;
-    private AdminCreateDTO admin;
 }
