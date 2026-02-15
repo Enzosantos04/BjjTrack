3e5a7061d776fcfb355a876df0fd5ab3610f95dc
enzosdev <enzohfs@gmail.com>
2026-02-15 19:54:37 -0300

    feat: create AdminCreateDTO as request

--- src/main/java/enzosdev/bjjtrack/dto/AdminCreateDTO.java
+++ src/main/java/enzosdev/bjjtrack/dto/AdminCreateDTO.java
@@ -1,4 +1,18 @@
 package enzosdev.bjjtrack.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class AdminCreateDTO {
+
+    private String name;
+    private String email;
+    private String password;
 }
