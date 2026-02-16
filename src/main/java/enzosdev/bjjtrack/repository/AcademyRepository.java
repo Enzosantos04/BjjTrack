3100322e3789029d1724ad10fca8dc8059d1dbb6
enzosdev <enzohfs@gmail.com>
2026-02-15 21:13:57 -0300

    fix: correct JpaRepository generic types Academy

--- src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AcademyRepository extends JpaRepository<Long, Academy> {
+public interface AcademyRepository extends JpaRepository<Academy, Long> {
 }
