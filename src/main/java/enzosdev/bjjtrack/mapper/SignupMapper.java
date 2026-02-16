b254a56f53a0ba35bfab9c19db8b87331d6d4430
enzosdev <enzohfs@gmail.com>
2026-02-15 21:01:00 -0300

    feat: add SignupMapper to map SignupResponse to response

--- src/main/java/enzosdev/bjjtrack/mapper/SignupMapper.java
+++ src/main/java/enzosdev/bjjtrack/mapper/SignupMapper.java
@@ -1,4 +1,16 @@
 package enzosdev.bjjtrack.mapper;
 
+import enzosdev.bjjtrack.dto.AcademyCreateResponseDTO;
+import enzosdev.bjjtrack.dto.AdminResponseDTO;
+import enzosdev.bjjtrack.dto.SignupResponse;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SignupMapper {
+    public SignupResponse  toResponse(AcademyCreateResponseDTO academy, AdminResponseDTO admin){
+       SignupResponse response = new SignupResponse();
+         response.setAcademy(academy);
+         response.setAdmin(admin);
+        return response;
+    }
 }
