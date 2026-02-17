8d0cc7f362480b8a93fdb552d911acd61532fae3
enzosdev <enzohfs@gmail.com>
2026-02-16 23:30:07 -0300

    feat: add ControllerExceptionHandler and handling MethodArgumentNotValidException

---
+++ src/main/java/enzosdev/bjjtrack/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package enzosdev.bjjtrack.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("Status", "400");
+        errorResponse.put("error", "Empty fields are not allowed, try again");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
