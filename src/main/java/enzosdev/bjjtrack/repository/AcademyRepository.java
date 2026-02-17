29c782cc0beee290f912d60ef6d7c39d43fc78ff
enzosdev <enzohfs@gmail.com>
2026-02-17 14:37:34 -0300

    feat: create derived queries existsBySlug and existsByName

--- src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
+        boolean existsBySlug(String slug);
+        boolean existsByName(String name);
 }
