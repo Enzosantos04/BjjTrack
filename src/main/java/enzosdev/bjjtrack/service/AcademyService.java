33fb3ff3d77a4e2ce44319cca35d12b2ac9ca852
enzosdev <enzohfs@gmail.com>
2026-02-18 20:17:05 -0300

    feat: add AcademyService with findAllAcademies method as BRs

--- src/main/java/enzosdev/bjjtrack/service/AcademyService.java
+++ src/main/java/enzosdev/bjjtrack/service/AcademyService.java
@@ -1,4 +1,29 @@
 package enzosdev.bjjtrack.service;
 
-public class AcademyServvice {
+import enzosdev.bjjtrack.dto.AcademyResponse;
+import enzosdev.bjjtrack.mapper.AcademyMapper;
+import enzosdev.bjjtrack.repository.AcademyRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcademyService {
+
+
+
+
+    private final AcademyRepository academyRepository;
+    private final AcademyMapper academyMapper;
+
+
+    public AcademyService(AcademyRepository academyRepository, AcademyMapper academyMapper) {
+        this.academyRepository = academyRepository;
+        this.academyMapper = academyMapper;
+    }
+
+    public Page<AcademyResponse> findAllAcademies(Pageable pageable){
+       return academyRepository.findAll(pageable)
+               .map(academyMapper::toResponse);
+    }
 }
