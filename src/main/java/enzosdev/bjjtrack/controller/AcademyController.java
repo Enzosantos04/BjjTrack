0ee22bfda510b6650f7ce8466e433e807ff23c33
enzosdev <enzohfs@gmail.com>
2026-02-18 20:39:04 -0300

    feat: add deleteAcademyById endpoint

--- src/main/java/enzosdev/bjjtrack/controller/AcademyController.java
+++ src/main/java/enzosdev/bjjtrack/controller/AcademyController.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/academy")
@@ -27,5 +25,11 @@ public class AcademyController {
         Page<AcademyResponse> academies = academyService.findAllAcademies(pageable);
         return ResponseEntity.status(HttpStatus.OK).body(academies);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAcademyById(@PathVariable Long id){
+        academyService.deleteAcademyById(id);
+        return ResponseEntity.noContent().build();
     }
 }
