a82d232fb3e2cbf4513ff569aadac8ae904af04d
enzosdev <enzohfs@gmail.com>
2026-02-19 13:00:48 -0300

    feat: add findAllUsersByAcademyId endpoint

--- src/main/java/enzosdev/bjjtrack/controller/AcademyController.java
+++ src/main/java/enzosdev/bjjtrack/controller/AcademyController.java
@@ -4,8 +4,10 @@ package enzosdev.bjjtrack.controller;
 import enzosdev.bjjtrack.dto.AcademyRequest;
 import enzosdev.bjjtrack.dto.AcademyResponse;
 import enzosdev.bjjtrack.dto.AcademyUpdateRequest;
+import enzosdev.bjjtrack.dto.UserResponse;
 import enzosdev.bjjtrack.service.AcademyService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -40,5 +42,12 @@ public class AcademyController {
     public ResponseEntity<AcademyResponse> updateAcademyById(@PathVariable Long id,@Valid @RequestBody AcademyUpdateRequest academyRequest){
         AcademyResponse academy= academyService.updateAcademyById(id, academyRequest);
         return ResponseEntity.status(HttpStatus.OK).body(academy);
+    }
+
+
+    @GetMapping("/{id}/users")
+    public ResponseEntity<Page<UserResponse>> findAllUsersByAcademyId(@PathVariable Long id, Pageable pageable){
+        Page<UserResponse> users = academyService.listUsersByAcademyId(id,pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 }
