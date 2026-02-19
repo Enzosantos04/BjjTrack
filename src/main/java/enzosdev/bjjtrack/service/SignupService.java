1fe8062746b1f98f24b89f4f4a8c2244f6321506
enzosdev <enzohfs@gmail.com>
2026-02-19 13:07:05 -0300

    refactor: renames UserMapper to AdminMapper and its instances

--- src/main/java/enzosdev/bjjtrack/service/SignupService.java
+++ src/main/java/enzosdev/bjjtrack/service/SignupService.java
@@ -1,6 +1,5 @@
 package enzosdev.bjjtrack.service;
 
-import enzosdev.bjjtrack.config.PasswordEncoderConfig;
 import enzosdev.bjjtrack.dto.AcademyResponse;
 import enzosdev.bjjtrack.dto.AdminResponse;
 import enzosdev.bjjtrack.dto.SignupRequest;
@@ -12,7 +11,7 @@ import enzosdev.bjjtrack.exceptions.AcademySlugAlreadyExistsException;
 import enzosdev.bjjtrack.exceptions.UserEmailAlreadyExistsException;
 import enzosdev.bjjtrack.mapper.AcademyMapper;
 import enzosdev.bjjtrack.mapper.SignupMapper;
-import enzosdev.bjjtrack.mapper.UserMapper;
+import enzosdev.bjjtrack.mapper.AdminMapper;
 import enzosdev.bjjtrack.repository.AcademyRepository;
 import enzosdev.bjjtrack.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,15 +24,15 @@ public class SignupService {
     private final AcademyRepository  academyRepository;
     private final UserRepository userRepository;
     private final SignupMapper signupMapper;
-    private final UserMapper userMapper;
+    private final AdminMapper adminMapper;
     private final AcademyMapper academyMapper;
     private final PasswordEncoder passwordEncoder;
 
-    public SignupService(AcademyRepository academyRepository, UserRepository userRepository, SignupMapper signupMapper, UserMapper userMapper, AcademyMapper academyMapper, PasswordEncoder passwordEncoder) {
+    public SignupService(AcademyRepository academyRepository, UserRepository userRepository, SignupMapper signupMapper, AdminMapper adminMapper, AcademyMapper academyMapper, PasswordEncoder passwordEncoder) {
         this.academyRepository = academyRepository;
         this.userRepository = userRepository;
         this.signupMapper = signupMapper;
-        this.userMapper = userMapper;
+        this.adminMapper = adminMapper;
         this.academyMapper = academyMapper;
         this.passwordEncoder = passwordEncoder;
     }
@@ -57,10 +56,10 @@ public class SignupService {
         Academy academy = academyMapper.toEntity(request.getAcademy());
         academy = academyRepository.save(academy);
         String hashedPassword = passwordEncoder.encode(request.getAdmin().getPassword());
-        User admin = userMapper.toAdminEntity(request.getAdmin(), academy, hashedPassword);
+        User admin = adminMapper.toAdminEntity(request.getAdmin(), academy, hashedPassword);
         admin = userRepository.save(admin);
         AcademyResponse academyResponse = academyMapper.toResponse(academy);
-        AdminResponse adminResponse = userMapper.toResponse(admin);
+        AdminResponse adminResponse = adminMapper.toResponse(admin);
         return signupMapper.toResponse(academyResponse, adminResponse);
     }
 
