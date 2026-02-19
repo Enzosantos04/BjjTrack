d2e1bf87ce1275210c993b5d5a2a3618daf9ae0c
enzosdev <enzohfs@gmail.com>
2026-02-19 13:03:24 -0300

    feat: add UserRequest as DTO

--- src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
@@ -1,4 +1,21 @@
 package enzosdev.bjjtrack.dto;
 
+
+import enzosdev.bjjtrack.entity.Academy;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private Academy academy;
+    private Long academyId;
 }
