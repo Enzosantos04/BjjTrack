bd2e06929cc609adec9a23d99b25dd16d929aa0d
enzosdev <enzohfs@gmail.com>
2026-02-19 19:53:29 -0300

    feat: create derived query on AcademyRepository to findAcademyBySlug

--- src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/AcademyRepository.java
@@ -1,11 +1,12 @@
 package enzosdev.bjjtrack.repository;
-
 import enzosdev.bjjtrack.entity.Academy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface AcademyRepository extends JpaRepository<Academy, Long> {
         boolean existsBySlug(String slug);
         boolean existsByName(String name);
+        Optional<Academy> findAcademyBySlugIgnoreCase(String slug);
 }
