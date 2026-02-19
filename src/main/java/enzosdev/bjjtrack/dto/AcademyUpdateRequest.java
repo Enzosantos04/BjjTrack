4cd9187ce65a11af8ea50082c1c482963bbac5ff
enzosdev <enzohfs@gmail.com>
2026-02-18 21:57:40 -0300

    feat: create Academy Resquest only to update to avoid Not null annotation

--- src/main/java/enzosdev/bjjtrack/dto/AcademyUpdateRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/AcademyUpdateRequest.java
@@ -1,4 +1,19 @@
 package enzosdev.bjjtrack.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class AcademyUpdateRequest {
+
+    private String name;
+    private String slug;
+    private String logoUrl;
+
 }
