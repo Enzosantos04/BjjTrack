babb3663be8f9dfd87b086eb257c914d4227c755
enzosdev <enzohfs@gmail.com>
2026-02-19 13:37:13 -0300

    feat: add AcademyNotFound to ControllerExceptionHandler

--- src/main/java/enzosdev/bjjtrack/exceptions/ControllerExceptionHandler.java
+++ src/main/java/enzosdev/bjjtrack/exceptions/ControllerExceptionHandler.java
@@ -1,10 +1,12 @@
 package enzosdev.bjjtrack.exceptions;
 
+import enzosdev.bjjtrack.dto.AcademyRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.sql.rowset.spi.SyncResolver;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,8 +24,8 @@ public class ControllerExceptionHandler {
 
 
     @ExceptionHandler(AcademyNameAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleAcademyNameAlreadyExistsException(AcademyNameAlreadyExistsException ex){
-        Map<String , String> response = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleAcademyNameAlreadyExistsException(AcademyNameAlreadyExistsException ex) {
+        Map<String, String> response = new HashMap<>();
         response.put("Message: ", ex.getMessage());
         response.put("Status", "400");
         response.put("error: ", "Academy name already exists, try again!");
@@ -32,8 +34,8 @@ public class ControllerExceptionHandler {
 
 
     @ExceptionHandler(AcademySlugAlreadyExistsException.class)
-    public ResponseEntity<Map<String,String>> handleAcademySlugAlreadyExistsException(AcademySlugAlreadyExistsException ex){
-        Map<String,String> response = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleAcademySlugAlreadyExistsException(AcademySlugAlreadyExistsException ex) {
+        Map<String, String> response = new HashMap<>();
         response.put("Message: ", ex.getMessage());
         response.put("Status", "400");
         response.put("error: ", "Academy slug already exists, try again!");
@@ -42,8 +44,8 @@ public class ControllerExceptionHandler {
 
 
     @ExceptionHandler(UserEmailAlreadyExistsException.class)
-    public ResponseEntity<Map<String,String >> handleUserEmailAlreadyExistsException(UserEmailAlreadyExistsException ex){
-        Map<String,String> response = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleUserEmailAlreadyExistsException(UserEmailAlreadyExistsException ex) {
+        Map<String, String> response = new HashMap<>();
         response.put("Message: ", ex.getMessage());
         response.put("Status", "400");
         response.put("error: ", "User email already exists, try again!");
@@ -51,11 +53,23 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(EmptyFieldException.class)
-    public ResponseEntity<Map<String,String >> handleEmptyFieldException(EmptyFieldException ex){
-        Map<String,String> response = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleEmptyFieldException(EmptyFieldException ex) {
+        Map<String, String> response = new HashMap<>();
         response.put("Message: ", ex.getMessage());
         response.put("Status", "400");
         response.put("error: ", "Empty Field are not allowed, try again!");
         return ResponseEntity.badRequest().body(response);
     }
+
+
+    @ExceptionHandler(AcademyNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleAcademyNotFoundException(AcademyNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("Message: ", ex.getMessage());
+        response.put("Status", "404");
+        response.put("error: ", "Academy Not Found");
+        return ResponseEntity.badRequest().body(response);
+    }
+
+
 }
