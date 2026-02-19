b6adf98c2d1ff72bc3b569ffa5e59c1c3499e23d
enzosdev <enzohfs@gmail.com>
2026-02-19 13:04:13 -0300

    feat: add UserResponse as return dto

--- src/main/java/enzosdev/bjjtrack/dto/UserResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/UserResponse.java
@@ -1,4 +1,18 @@
 package enzosdev.bjjtrack.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Long academyId;
 }
