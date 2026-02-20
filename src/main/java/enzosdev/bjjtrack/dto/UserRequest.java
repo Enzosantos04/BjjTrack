83129eb9b9e29073f4c56aa5a1422b1afbdf6933
enzosdev <enzohfs@gmail.com>
2026-02-19 22:50:37 -0300

    feat: add validation annotation to avoid empty fields

--- src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
@@ -2,6 +2,7 @@ package enzosdev.bjjtrack.dto;
 
 
 import enzosdev.bjjtrack.entity.Academy;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,11 @@ import lombok.Setter;
 @Setter
 public class UserRequest {
 
+    @NotBlank(message = "User's name is required")
     private String name;
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
     private Academy academy;
     private Long academyId;
