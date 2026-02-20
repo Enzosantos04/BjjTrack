5392fd77666cc6e21cec473bd64bb9a3a3453ad4
enzosdev <enzohfs@gmail.com>
2026-02-19 22:55:36 -0300

    feat: add createUser endpoint

--- src/main/java/enzosdev/bjjtrack/controller/UserController.java
+++ src/main/java/enzosdev/bjjtrack/controller/UserController.java
@@ -1,4 +1,29 @@
 package enzosdev.bjjtrack.controller;
 
+import enzosdev.bjjtrack.dto.UserRequest;
+import enzosdev.bjjtrack.dto.UserResponse;
+import enzosdev.bjjtrack.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest){
+        UserResponse userResponse = userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+    }
 }
