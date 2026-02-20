c58d1cf88c7fa008d7b462bc94d5de000613fbf8
enzosdev <enzohfs@gmail.com>
2026-02-19 23:28:38 -0300

    refactor: instance Academy entity and validation to CreateUser methodd

--- src/main/java/enzosdev/bjjtrack/service/UserService.java
+++ src/main/java/enzosdev/bjjtrack/service/UserService.java
@@ -4,11 +4,16 @@ import enzosdev.bjjtrack.dto.AcademyRequest;
 import enzosdev.bjjtrack.dto.AcademyResponse;
 import enzosdev.bjjtrack.dto.UserRequest;
 import enzosdev.bjjtrack.dto.UserResponse;
+import enzosdev.bjjtrack.entity.Academy;
 import enzosdev.bjjtrack.entity.User;
+import enzosdev.bjjtrack.exceptions.AcademyNotFoundException;
 import enzosdev.bjjtrack.mapper.UserMapper;
+import enzosdev.bjjtrack.repository.AcademyRepository;
 import enzosdev.bjjtrack.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,16 +21,20 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final AcademyRepository academyRepository;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, AcademyRepository academyRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
+        this.academyRepository = academyRepository;
     }
 
     public UserResponse createUser(UserRequest userRequest){
+        Academy academy = academyRepository.findById(userRequest.getAcademyId())
+                .orElseThrow(() -> new AcademyNotFoundException("Academy not found"));
         String hashedPassword = passwordEncoder.encode(userRequest.getPassword());
-        User user = userMapper.toUserEntity(userRequest, hashedPassword);
+        User user = userMapper.toUserEntity(userRequest, academy, hashedPassword);
         user = userRepository.save(user);
         return userMapper.toResponse(user);
     }
