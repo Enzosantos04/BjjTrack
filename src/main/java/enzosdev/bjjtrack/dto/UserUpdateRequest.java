959b3a7185df4b04c32e144f8b41f6083aa779e4
enzosdev <enzohfs@gmail.com>
2026-02-26 21:57:26 -0300

    feat: add dto to update user without validation annotations

--- src/main/java/enzosdev/bjjtrack/dto/UserUpdateRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/UserUpdateRequest.java
@@ -1,4 +1,17 @@
 package enzosdev.bjjtrack.dto;
 
+
+import enzosdev.bjjtrack.entity.Academy;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserUpdateRequest {
+
+    private String name;
+    private String email;
+    private Academy academy;
+    private Long academyId;
 }
