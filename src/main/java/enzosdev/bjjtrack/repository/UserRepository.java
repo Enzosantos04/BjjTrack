d4cffb4ad53c16e70ceb8674a9dba00fb0e26054
enzosdev <enzohfs@gmail.com>
2026-02-26 21:58:45 -0300

    feat: create derived query to find user by academy id

--- src/main/java/enzosdev/bjjtrack/repository/UserRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package enzosdev.bjjtrack.repository;
 
+import enzosdev.bjjtrack.entity.Academy;
 import enzosdev.bjjtrack.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+   Page<User> findByAcademyId(Long id, Pageable pageable);
 }
