b4ddc3ba22378405fdb2c79aaab2ead9a34b2f76
enzosdev <enzohfs@gmail.com>
2026-03-02 18:36:13 -0300

    refactor: add ignoreCase on derived query to findUserByEmail

--- src/main/java/enzosdev/bjjtrack/repository/UserRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/UserRepository.java
@@ -13,5 +13,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
    Page<User> findByAcademyId(Long id, Pageable pageable);
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByEmailIgnoreCase(String email);
 }
