b6c62a6653339945991884b81c21589c6c991478
enzosdev <enzohfs@gmail.com>
2026-03-03 22:04:17 -0300

    feat: create UserAlreadyActivatedException

---
+++ src/main/java/enzosdev/bjjtrack/exceptions/UserAlreadyDeactivatedException.java
@@ -0,0 +1,7 @@
+package enzosdev.bjjtrack.exceptions;
+
+public class UserAlreadyDeactivatedException extends RuntimeException {
+  public UserAlreadyDeactivatedException(String message) {
+    super(message);
+  }
+}
