10e3f4394962e90df28afe4cd05d3aa07843a0a5
enzosdev <enzohfs@gmail.com>
2026-03-11 21:35:28 -0300

    refactor: removes academy entity from dto to better practices

--- src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/UserRequest.java
@@ -22,6 +22,5 @@ public class UserRequest {
     private String email;
     @NotBlank(message = "Password is required")
     private String password;
-    private Academy academy;
     private Long academyId;
 }
