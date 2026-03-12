86f7ab009c78b4bfe5fae949f909af4989a233db
enzosdev <enzohfs@gmail.com>
2026-03-11 21:18:09 -0300

    feat: add attributes to StudentRequest

--- src/main/java/enzosdev/bjjtrack/dto/StudentRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/StudentRequest.java
@@ -1,4 +1,36 @@
 package enzosdev.bjjtrack.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import enzosdev.bjjtrack.entity.Academy;
+import enzosdev.bjjtrack.entity.User;
+import enzosdev.bjjtrack.enums.Belt;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StudentRequest {
+
+    @NotNull(message = "User id is required")
+    private Long userId;
+
+    @NotNull(message = "Academy id is required")
+    private Long academyId;
+
+    @NotNull(message = "Birth date is required")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+
+    private Belt belt;
+
 }
