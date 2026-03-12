35b7a8532bf74faba29b985a6c865b91e7bca8af
enzosdev <enzohfs@gmail.com>
2026-03-11 21:18:36 -0300

    feat: add attributes to StudentResponse

--- src/main/java/enzosdev/bjjtrack/dto/StudentResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/StudentResponse.java
@@ -1,4 +1,21 @@
 package enzosdev.bjjtrack.dto;
 
+import enzosdev.bjjtrack.enums.Belt;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StudentResponse {
+
+    private Long userId;
+    private Long academyId;
+    private LocalDate birthDate;
+    private Belt belt;
 }
