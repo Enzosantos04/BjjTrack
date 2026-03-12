a371e98c68f042f9cadff1c77c3e61e2f3001a62
enzosdev <enzohfs@gmail.com>
2026-03-11 21:57:47 -0300

    feat: add createStudent endpoint

--- src/main/java/enzosdev/bjjtrack/controller/StudentController.java
+++ src/main/java/enzosdev/bjjtrack/controller/StudentController.java
@@ -1,9 +1,28 @@
 package enzosdev.bjjtrack.controller;
 
+import enzosdev.bjjtrack.dto.StudentRequest;
+import enzosdev.bjjtrack.dto.StudentResponse;
+import enzosdev.bjjtrack.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+    private StudentService studentService;
+
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    public ResponseEntity<StudentResponse> createStudent(@Valid @RequestBody StudentRequest studentRequest){
+        StudentResponse studentResponse = studentService.createStudent(studentRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentResponse);
+    }
 }
