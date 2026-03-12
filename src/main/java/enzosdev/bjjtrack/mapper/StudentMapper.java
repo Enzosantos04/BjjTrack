ef591d29d3d6f0897c92ecddaf22bba9872dc221
enzosdev <enzohfs@gmail.com>
2026-03-11 21:44:56 -0300

    feat: on StudentMapper maps StudentRequest toEntity and Student entity toResponse

--- src/main/java/enzosdev/bjjtrack/mapper/StudentMapper.java
+++ src/main/java/enzosdev/bjjtrack/mapper/StudentMapper.java
@@ -1,7 +1,45 @@
 package enzosdev.bjjtrack.mapper;
 
+import enzosdev.bjjtrack.dto.StudentRequest;
+import enzosdev.bjjtrack.dto.StudentResponse;
+import enzosdev.bjjtrack.entity.Academy;
+import enzosdev.bjjtrack.entity.Student;
+import enzosdev.bjjtrack.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StudentMapper {
+
+    public Student toEntity(StudentRequest studentRequest, Academy academy, User user){
+        Student student = new Student();
+        student.setAcademy(academy);
+        student.setUser(user);
+        student.setBelt(studentRequest.getBelt());
+        student.setBrithDate(studentRequest.getBirthDate());
+        return student;
+
+
+    }
+
+
+    public StudentResponse toResponse(Student student){
+        if(student == null){
+            return null;
+        }
+
+        StudentResponse dto = new StudentResponse();
+        dto.setId(student.getId());
+        dto.setBirthDate(student.getBrithDate());
+        dto.setBelt(student.getBelt());
+        if (student.getAcademy() != null){
+            dto.setAcademyId(student.getAcademy().getId());
+        }
+
+        if (student.getUser() != null){
+            dto.setUserId(student.getUser().getId());
+        }
+
+        return dto;
+
+    }
 }
