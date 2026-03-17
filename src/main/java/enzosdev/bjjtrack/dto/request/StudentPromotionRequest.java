4c50670f25ebcf0c3a135be9d9843bc1a2e71376
enzosdev <enzohfs@gmail.com>
2026-03-17 20:22:13 -0300

    feat: add lastPromotion to StudentPromotionRequest

--- src/main/java/enzosdev/bjjtrack/dto/request/StudentPromotionRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/StudentPromotionRequest.java
@@ -1,4 +1,22 @@
 package enzosdev.bjjtrack.dto.request;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentPromotionRequest {
+
+    @NotNull(message = "Last promotion date is required")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate lastPromotion;
 }
