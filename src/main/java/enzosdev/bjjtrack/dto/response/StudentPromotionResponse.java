aad1ce2d49995b1a20d2bf66087e69acab7f4ee9
enzosdev <enzohfs@gmail.com>
2026-03-17 20:23:41 -0300

    feat: create StudentPromotionResponse

--- src/main/java/enzosdev/bjjtrack/dto/response/StudentPromotionResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/response/StudentPromotionResponse.java
@@ -1,4 +1,22 @@
 package enzosdev.bjjtrack.dto.response;
 
+
+import enzosdev.bjjtrack.enums.Belt;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentPromotionResponse {
+    private Long studentId;
+    private Integer stripes;
+    private Belt belt;
+    private LocalDate lastPromotion;
+
 }
