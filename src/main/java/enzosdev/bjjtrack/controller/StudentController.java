d9db68b78eb77310d71e429394db423584adf529
enzosdev <enzohfs@gmail.com>
2026-03-17 21:33:14 -0300

    feat: create promoteStudentStripe endpoint

--- src/main/java/enzosdev/bjjtrack/controller/StudentController.java
+++ src/main/java/enzosdev/bjjtrack/controller/StudentController.java
@@ -1,15 +1,14 @@
 package enzosdev.bjjtrack.controller;
 
+import enzosdev.bjjtrack.dto.request.StudentPromotionRequest;
 import enzosdev.bjjtrack.dto.request.StudentRequest;
+import enzosdev.bjjtrack.dto.response.StudentPromotionResponse;
 import enzosdev.bjjtrack.dto.response.StudentResponse;
 import enzosdev.bjjtrack.service.StudentService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/students")
@@ -26,5 +25,11 @@ public class StudentController {
     public ResponseEntity<StudentResponse> createStudent(@Valid @RequestBody StudentRequest studentRequest){
         StudentResponse studentResponse = studentService.createStudent(studentRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(studentResponse);
+    }
+
+    @PatchMapping("/{id}/stripe")
+    ResponseEntity<StudentPromotionResponse> promoteStudentStripe(@PathVariable Long id, @RequestBody StudentPromotionRequest promotionRequest){
+        StudentPromotionResponse promotionResponse = studentService.promoteStripe(id, promotionRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(promotionResponse);
     }
 }
