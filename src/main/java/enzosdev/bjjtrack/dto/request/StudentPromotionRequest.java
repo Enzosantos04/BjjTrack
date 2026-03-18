52329b4c0db73bbff9437dc0b6a3c9f44c08fa7b
enzosdev <enzohfs@gmail.com>
2026-03-18 20:53:04 -0300

    feat: add belt attribute to StudentPromotionRequest

--- src/main/java/enzosdev/bjjtrack/dto/request/StudentPromotionRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/StudentPromotionRequest.java
@@ -2,6 +2,9 @@ package enzosdev.bjjtrack.dto.request;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import enzosdev.bjjtrack.enums.Belt;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,4 +22,6 @@ public class StudentPromotionRequest {
     @NotNull(message = "Last promotion date is required")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate lastPromotion;
+    @Enumerated(EnumType.STRING)
+    private Belt belt;
 }
