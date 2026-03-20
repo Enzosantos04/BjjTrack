335e8cfc4330f55390f783a24216ce0c558a21bc
enzosdev <enzohfs@gmail.com>
2026-03-20 12:03:45 -0300

    feat: create derived query to findStudentsByAcademyId

--- src/main/java/enzosdev/bjjtrack/repository/StudentRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/StudentRepository.java
@@ -1,10 +1,13 @@
 package enzosdev.bjjtrack.repository;
 
 import enzosdev.bjjtrack.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsStudentByUserId(Long userId);
+    Page<Student> findStudentsByAcademyId(Long academyId, Pageable pageable);
 }
