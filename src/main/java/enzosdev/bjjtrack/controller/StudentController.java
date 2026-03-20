8c480d0753596ae8a730d01c849c8f0ade66a5e9
enzosdev <enzohfs@gmail.com>
2026-03-20 11:31:18 -0300

    feat: create findAllStudent endpoint

--- src/main/java/enzosdev/bjjtrack/controller/StudentController.java
+++ src/main/java/enzosdev/bjjtrack/controller/StudentController.java
@@ -6,6 +6,8 @@ import enzosdev.bjjtrack.dto.response.StudentPromotionResponse;
 import enzosdev.bjjtrack.dto.response.StudentResponse;
 import enzosdev.bjjtrack.service.StudentService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +40,13 @@ public class StudentController {
         StudentPromotionResponse studentResponse = studentService.promoteBelt(id, promotionRequest);
         return ResponseEntity.ok(studentResponse);
 
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<StudentResponse>> findAllStudents(Pageable pageable){
+       Page<StudentResponse> studentResponse = studentService.findAllStudents(pageable);
+       return ResponseEntity.ok(studentResponse);
     }
 
 }
