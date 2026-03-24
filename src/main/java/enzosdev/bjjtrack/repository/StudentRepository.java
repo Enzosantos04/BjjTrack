3eb85603298b50e8d1c0995bb34dc9ad60e8c515
enzosdev <enzohfs@gmail.com>
2026-03-24 20:04:16 -0300

    feat: add derived query to find students by user email

--- src/main/java/enzosdev/bjjtrack/repository/StudentRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/StudentRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsStudentByUserId(Long userId);
     Page<Student> findStudentsByAcademyId(Long academyId, Pageable pageable);
     Page<Student> findAllStudentsByUserActiveTrue(Pageable pageable);
+    Optional<Student> findStudentByUserEmail(String string);
 }
