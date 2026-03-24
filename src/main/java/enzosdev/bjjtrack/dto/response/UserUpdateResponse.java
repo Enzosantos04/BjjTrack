76b4160ccd90aaffbc130fbc79e99064a380fc1f
enzosdev <enzohfs@gmail.com>
2026-03-24 15:32:46 -0300

    feat: add UserUpdateResponse

--- src/main/java/enzosdev/bjjtrack/dto/response/UserUpdateResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/response/UserUpdateResponse.java
@@ -1,4 +1,18 @@
 package enzosdev.bjjtrack.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserUpdateResponse {
+
+    private Long id;
+    private String name;
+    private Long academyId;
 }
