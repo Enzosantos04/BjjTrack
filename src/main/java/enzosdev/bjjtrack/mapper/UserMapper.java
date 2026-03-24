d739e642b7634e615aa7009a8c5302ceab38dab7
enzosdev <enzohfs@gmail.com>
2026-03-24 15:46:46 -0300

    feat: add toUpdateResponse mapper

--- src/main/java/enzosdev/bjjtrack/mapper/UserMapper.java
+++ src/main/java/enzosdev/bjjtrack/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package enzosdev.bjjtrack.mapper;
 
 import enzosdev.bjjtrack.dto.request.UserRequest;
 import enzosdev.bjjtrack.dto.response.UserResponse;
+import enzosdev.bjjtrack.dto.response.UserUpdateResponse;
 import enzosdev.bjjtrack.entity.Academy;
 import enzosdev.bjjtrack.entity.User;
 import org.springframework.stereotype.Component;
@@ -28,6 +29,22 @@ public class UserMapper {
         dto.setId(user.getId());
         dto.setName(user.getName());
         dto.setEmail(user.getEmail());
+        if (user.getAcademy() != null) {
+            dto.setAcademyId(user.getAcademy().getId());
+        }
+        return dto;
+
+    }
+
+
+    public UserUpdateResponse toUpdateResponse(User user){
+        if (user == null){
+            return null;
+        }
+
+        UserUpdateResponse dto = new UserUpdateResponse();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
         if (user.getAcademy() != null) {
             dto.setAcademyId(user.getAcademy().getId());
         }
