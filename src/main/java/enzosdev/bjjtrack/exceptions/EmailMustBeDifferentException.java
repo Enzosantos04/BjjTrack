7ec96e748c79004d231d0aab27ecb21457fe6ce5
enzosdev <enzohfs@gmail.com>
2026-03-24 19:14:55 -0300

    feat: add email must be differente exception to avoid updating old email as new

--- src/main/java/enzosdev/bjjtrack/exceptions/EmailMustBeDifferentException.java
+++ src/main/java/enzosdev/bjjtrack/exceptions/EmailMustBeDifferentException.java
@@ -1,7 +1,7 @@
 package enzosdev.bjjtrack.exceptions;
 
 public class EmailMustBeDifferentException extends RuntimeException {
-  public EmailMustBeDifferentException(String message) {
-    super(message);
-  }
+    public EmailMustBeDifferentException(String message) {
+        super(message);
+    }
 }
