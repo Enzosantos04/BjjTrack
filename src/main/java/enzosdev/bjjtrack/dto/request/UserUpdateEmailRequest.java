1dd1e7f3d4e4ee1d0c2022a6bc36608f9cb0d583
enzosdev <enzohfs@gmail.com>
2026-03-24 19:07:45 -0300

    feat: add validation annotations

--- src/main/java/enzosdev/bjjtrack/dto/request/UserUpdateEmailRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/UserUpdateEmailRequest.java
@@ -1,5 +1,7 @@
 package enzosdev.bjjtrack.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class UpdateUserEmailRequest {
+public class UserUpdateEmailRequest {
     private Long id;
+
+    @Email(message = "This Email is in wrong format")
     private String newEmail;
+    @NotBlank(message = "password is required")
     private String password;
 }
