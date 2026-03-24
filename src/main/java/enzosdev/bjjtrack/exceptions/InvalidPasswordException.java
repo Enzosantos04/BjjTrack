6c65ea7ec24c1a705d5c884abfad45d2d59f69b0
enzosdev <enzohfs@gmail.com>
2026-03-24 19:13:39 -0300

    feat: add Invalid password exception

--- src/main/java/enzosdev/bjjtrack/exceptions/InvalidPasswordException.java
+++ src/main/java/enzosdev/bjjtrack/exceptions/InvalidPasswordException.java
@@ -1,7 +1,7 @@
 package enzosdev.bjjtrack.exceptions;
 
 public class InvalidPasswordException extends RuntimeException {
-  public InvalidPasswordException(String message) {
-    super(message);
-  }
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
 }
