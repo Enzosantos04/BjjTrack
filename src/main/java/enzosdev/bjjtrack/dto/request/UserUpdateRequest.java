dda257a9890e103d0c16c4eef49820ec2020c7cd
enzosdev <enzohfs@gmail.com>
2026-03-24 19:08:44 -0300

    feat: add validation annotations

--- src/main/java/enzosdev/bjjtrack/dto/request/UserUpdateRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/UserUpdateRequest.java
@@ -3,6 +3,8 @@ package enzosdev.bjjtrack.dto.request;
 
 import enzosdev.bjjtrack.entity.Academy;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -11,7 +13,10 @@ import lombok.*;
 @NoArgsConstructor
 public class UserUpdateRequest {
 
+    @NotBlank
+    @NotBlank(message = "name is required")
     private String name;
     private Academy academy;
+    @NotNull(message = "academyId is required")
     private Long academyId;
 }
