64643be23ca835c956af1ac31c8e64d2389463cd
enzosdev <enzohfs@gmail.com>
2026-03-29 00:28:27 -0300

    feat: add dto StudenUpdateRequest

--- src/main/java/enzosdev/bjjtrack/dto/request/StudentUpdateRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/StudentUpdateRequest.java
@@ -1,4 +1,30 @@
 package enzosdev.bjjtrack.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import enzosdev.bjjtrack.enums.Belt;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StudentUpdateRequest {
+
+
+
+    @NotNull(message = "Birth date is required")
+    @Past(message = "Birth date must be in the past")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+
+    @NotNull(message = "Belt is required")
+    private Belt belt;
+    private Integer stripes;
 }
