11439a6e1b252293c5ff5f79541c11c1697f6b79
enzosdev <enzohfs@gmail.com>
2026-04-04 18:20:42 -0300

    feat: add responses for StudentAdminUpdate and StudentProfileUpdate

--- src/main/java/enzosdev/bjjtrack/dto/response/StudentAdminUpdateResponse.java
+++ src/main/java/enzosdev/bjjtrack/dto/response/StudentAdminUpdateResponse.java
@@ -1,4 +1,17 @@
 package enzosdev.bjjtrack.dto.response;
 
+import enzosdev.bjjtrack.enums.Belt;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentAdminUpdateResponse {
+
+    private Belt belt;
+    private Integer stripe;
 }
