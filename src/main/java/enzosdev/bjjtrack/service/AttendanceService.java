e37bbea7a49ba6cbff19499ce1e98972488c70c5
enzosdev <enzohfs@gmail.com>
2026-04-26 20:53:18 -0300

    refactor: replace run time exception to AttendanceNotFound and AttendanceAlreadyExists exceptions

--- src/main/java/enzosdev/bjjtrack/service/AttendanceService.java
+++ src/main/java/enzosdev/bjjtrack/service/AttendanceService.java
@@ -5,7 +5,8 @@ import enzosdev.bjjtrack.dto.request.AttendanceRequest;
 import enzosdev.bjjtrack.dto.response.AttendanceResponse;
 import enzosdev.bjjtrack.entity.Attendance;
 import enzosdev.bjjtrack.entity.Student;
-import enzosdev.bjjtrack.exceptions.EmptyFieldException;
+import enzosdev.bjjtrack.exceptions.AttendanceAlreadyExistsException;
+import enzosdev.bjjtrack.exceptions.AttendanceNotFoundException;
 import enzosdev.bjjtrack.exceptions.StudentNotFoundException;
 import enzosdev.bjjtrack.mapper.AttendanceMapper;
 import enzosdev.bjjtrack.repository.AttendanceRepository;
@@ -28,20 +29,20 @@ public class AttendanceService {
 
     public AttendanceResponse createAttendance(AttendanceRequest attendanceRequest) {
         Student student = studentRepository.findById(attendanceRequest.getStudentId())
-                .orElseThrow(() -> new StudentNotFoundException("Student not found"));
+                .orElseThrow(() -> new StudentNotFoundException("Student with id " + attendanceRequest.getStudentId() + " not found"));
 
-      if (attendanceRepository.existsByStudentIdAndAttendanceDate(attendanceRequest.getStudentId(),  attendanceRequest.getAttendanceDate())) {
-          throw  new RuntimeException("Attendance already exists");
-      }
+        if (attendanceRepository.existsByStudentIdAndAttendanceDate(attendanceRequest.getStudentId(), attendanceRequest.getAttendanceDate())) {
+            throw new AttendanceAlreadyExistsException("Attendance already registered for this date");
+        }
+
         Attendance attendance = attendanceMapper.toEntity(attendanceRequest, student);
         attendanceRepository.save(attendance);
         return attendanceMapper.toResponse(attendance);
-
     }
 
     public void deleteAttendance(Long id) {
         if (!attendanceRepository.existsById(id)) {
-            throw new RuntimeException("Attendance not found");
+            throw new AttendanceNotFoundException("Attendance with id " + id + " not found");
         }
         attendanceRepository.deleteById(id);
     }
