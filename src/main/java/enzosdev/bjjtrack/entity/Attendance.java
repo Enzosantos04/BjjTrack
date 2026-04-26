54b6e070d64045ee3c0aa441bd448fa27e24220b
enzosdev <enzohfs@gmail.com>
2026-04-26 00:04:42 -0300

    feat: add attendance entity

---
+++ src/main/java/enzosdev/bjjtrack/entity/Attendance.java
@@ -0,0 +1,27 @@
+package enzosdev.bjjtrack.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "attendance")
+@Builder
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @Column(name = "attendance_date", nullable = false)
+    private LocalDate attendanceDate;
+
+}
