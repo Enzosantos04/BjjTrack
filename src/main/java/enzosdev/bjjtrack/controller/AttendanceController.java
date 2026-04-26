eba4484b085b4a050a658fa6c94aa5a478fa6642
enzosdev <enzohfs@gmail.com>
2026-04-26 20:42:11 -0300

    feat: add attendance endpoint

--- src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
+++ src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
@@ -1,4 +1,30 @@
 package enzosdev.bjjtrack.controller;
 
+import enzosdev.bjjtrack.dto.request.AttendanceRequest;
+import enzosdev.bjjtrack.dto.response.AttendanceResponse;
+import enzosdev.bjjtrack.service.AttendanceService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class AttendanceController {
+
+    private final AttendanceService attendanceService;
+
+    public AttendanceController(AttendanceService attendanceService) {
+        this.attendanceService = attendanceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AttendanceResponse> attendance(@Valid @RequestBody AttendanceRequest attendanceRequest) {
+        AttendanceResponse attendanceResponse = attendanceService.createAttendance(attendanceRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(attendanceResponse);
+
+    }
 }
