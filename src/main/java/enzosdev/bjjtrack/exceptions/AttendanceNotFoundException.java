4d5f255282f8cbfe28daf37b494e2dd6b101e1bd
enzosdev <enzohfs@gmail.com>
2026-04-26 20:54:47 -0300

    feat: add attendace not found exception

---
+++ src/main/java/enzosdev/bjjtrack/exceptions/AttendanceNotFoundException.java
@@ -0,0 +1,7 @@
+package enzosdev.bjjtrack.exceptions;
+
+public class AttendanceNotFoundException extends RuntimeException {
+    public AttendanceNotFoundException(String message) {
+        super(message);
+    }
+}
