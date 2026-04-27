f4cec4880bb1a032e5b9b3ad6b7dfb6f9a9062e0
enzosdev <enzohfs@gmail.com>
2026-04-27 20:25:23 -0300

    feat: add findAttendanceByAcademyId derived query

--- src/main/java/enzosdev/bjjtrack/repository/AttendanceRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/AttendanceRepository.java
@@ -1,6 +1,8 @@
 package enzosdev.bjjtrack.repository;
 
 import enzosdev.bjjtrack.entity.Attendance;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,5 @@ import java.time.LocalDate;
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     
     boolean existsByStudentIdAndAttendanceDate(Long studentId, LocalDate attendanceDate);
-    
+    Page<Attendance> findByStudentAcademyId(Long academyId, Pageable pageable);
 }
