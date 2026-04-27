461242f9f4dd3a6622653493440f06bf3cf885b0
enzosdev <enzohfs@gmail.com>
2026-04-26 21:04:22 -0300

    feat: add creatAttendace and deleteAttendance endpoints

--- src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
+++ src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
@@ -6,13 +6,10 @@ import enzosdev.bjjtrack.service.AttendanceService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/attendances")
 public class AttendanceController {
 
     private final AttendanceService attendanceService;
@@ -22,9 +19,14 @@ public class AttendanceController {
     }
 
     @PostMapping
-    public ResponseEntity<AttendanceResponse> attendance(@Valid @RequestBody AttendanceRequest attendanceRequest) {
-        AttendanceResponse attendanceResponse = attendanceService.createAttendance(attendanceRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(attendanceResponse);
+    public ResponseEntity<AttendanceResponse> createAttendance(@Valid @RequestBody AttendanceRequest attendanceRequest) {
+        AttendanceResponse response = attendanceService.createAttendance(attendanceRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAttendance(@PathVariable Long id) {
+        attendanceService.deleteAttendance(id);
+        return ResponseEntity.noContent().build();
     }
 }
