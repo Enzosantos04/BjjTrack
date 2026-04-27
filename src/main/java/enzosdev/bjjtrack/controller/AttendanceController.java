6c185826950877c4ccc737b4f24986b3dd3bd905
enzosdev <enzohfs@gmail.com>
2026-04-27 20:28:27 -0300

    feat: add findAttendaceByAcademyId endpoint

--- src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
+++ src/main/java/enzosdev/bjjtrack/controller/AttendanceController.java
@@ -4,6 +4,8 @@ import enzosdev.bjjtrack.dto.request.AttendanceRequest;
 import enzosdev.bjjtrack.dto.response.AttendanceResponse;
 import enzosdev.bjjtrack.service.AttendanceService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,12 @@ public class AttendanceController {
     public ResponseEntity<AttendanceResponse> createAttendance(@Valid @RequestBody AttendanceRequest attendanceRequest) {
         AttendanceResponse response = attendanceService.createAttendance(attendanceRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/academy/{id}")
+    public ResponseEntity<Page<AttendanceResponse>> findAttendancesByAcademyId(@PathVariable Long id, Pageable pageable) {
+        Page<AttendanceResponse> response = attendanceService.findAttendancesByAcademyId(id, pageable);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
