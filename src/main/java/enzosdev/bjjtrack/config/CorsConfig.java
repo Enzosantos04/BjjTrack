788ce244e70fca2aca57a770519582840062d2bf
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-04-30 19:12:15 -0300

    feat: create CorsConfig class to handle CORS settings

--- src/main/java/enzosdev/bjjtrack/config/CorsConfig.java
+++ src/main/java/enzosdev/bjjtrack/config/CorsConfig.java
@@ -1,8 +1,25 @@
 package enzosdev.bjjtrack.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+
+    @Bean
+    //cors configuration diz quem pode fazer a requisicao no backend
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+
+    }
 }
