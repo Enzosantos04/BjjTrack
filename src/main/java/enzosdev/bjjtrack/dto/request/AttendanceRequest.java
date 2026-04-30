d3645f8dcf827e4f84bbf6c05b2af887488605d0
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-04-30 13:12:34 -0300

    feat: add pastOrPresent annotation to avoid create attendance in the future

--- src/main/java/enzosdev/bjjtrack/dto/request/AttendanceRequest.java
+++ src/main/java/enzosdev/bjjtrack/dto/request/AttendanceRequest.java
@@ -2,6 +2,7 @@ package enzosdev.bjjtrack.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class AttendanceRequest {
     @NotNull(message = "Student id is required")
     private Long studentId;
 
+    @PastOrPresent(message = "Attendance date cannot be in the future")
     @NotNull(message = "Attendance date is required")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate attendanceDate;
