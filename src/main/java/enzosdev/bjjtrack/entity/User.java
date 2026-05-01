714548f40fa537abb4e5992e86a7af92c4187c10
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 17:24:00 -0300

    feat: implement join users_scopes table on user entity join user_id and scope_id

--- src/main/java/enzosdev/bjjtrack/entity/User.java
+++ src/main/java/enzosdev/bjjtrack/entity/User.java
@@ -4,6 +4,9 @@ package enzosdev.bjjtrack.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
@@ -24,4 +27,12 @@ public class User {
     @JoinColumn(name = "academy_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private Academy academy;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_scopes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "scope_id")
+    )
+    List<Scope> scopes = new ArrayList<>();
 }
