534b856007c79151872ca24c3bfaf7bcd2e8bc1c
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 17:14:36 -0300

    feat: create scope entity

--- src/main/java/enzosdev/bjjtrack/entity/Scope.java
+++ src/main/java/enzosdev/bjjtrack/entity/Scope.java
@@ -1,4 +1,21 @@
 package enzosdev.bjjtrack.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "scopes")
 public class Scope {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
