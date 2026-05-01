c7a25c1c94789e12d7ceb04fb655d058e28de54b
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 17:48:15 -0300

    feat: create ScopeService class and add find scope by id method as BRs

--- src/main/java/enzosdev/bjjtrack/service/ScopeService.java
+++ src/main/java/enzosdev/bjjtrack/service/ScopeService.java
@@ -1,4 +1,21 @@
 package enzosdev.bjjtrack.service;
 
+import enzosdev.bjjtrack.entity.Scope;
+import enzosdev.bjjtrack.repository.ScopeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ScopeService {
+
+    private final ScopeRepository scopeRepository;
+
+
+    public ScopeService(ScopeRepository scopeRepository) {
+        this.scopeRepository = scopeRepository;
+    }
+
+    public Scope findById(Long id) {
+        return scopeRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Scope with id " + id + " not found"));
+    }
 }
