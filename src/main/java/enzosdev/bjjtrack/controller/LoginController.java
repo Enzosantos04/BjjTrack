2016708230949d34c9ce7e259a6adbccd4a85a21
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 17:32:41 -0300

    feat: add login endpoint

--- src/main/java/enzosdev/bjjtrack/controller/LoginController.java
+++ src/main/java/enzosdev/bjjtrack/controller/LoginController.java
@@ -1,4 +1,30 @@
 package enzosdev.bjjtrack.controller;
 
+
+import enzosdev.bjjtrack.dto.request.LoginRequest;
+import enzosdev.bjjtrack.dto.response.LoginResponse;
+import enzosdev.bjjtrack.service.LoginService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        LoginResponse response = loginService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
