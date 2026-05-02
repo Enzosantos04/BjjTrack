b45161c6a17b070b1f9de4e6eee0e5dbc7884f84
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 23:06:40 -0300

    feat: add scope not found exception

---
+++ src/main/java/enzosdev/bjjtrack/exceptions/ScopeNotFoundException.java
@@ -0,0 +1,7 @@
+package enzosdev.bjjtrack.exceptions;
+
+public class ScopeNotFoundException extends RuntimeException {
+    public ScopeNotFoundException(String message) {
+        super(message);
+    }
+}
