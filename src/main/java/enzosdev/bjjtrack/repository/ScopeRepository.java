722b4b96703ac5567c0a5ac24ae56dd3e1b044bf
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-01 22:07:20 -0300

    feat: add find scope by name derived query

--- src/main/java/enzosdev/bjjtrack/repository/ScopeRepository.java
+++ src/main/java/enzosdev/bjjtrack/repository/ScopeRepository.java
@@ -3,5 +3,8 @@ package enzosdev.bjjtrack.repository;
 import enzosdev.bjjtrack.entity.Scope;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ScopeRepository extends JpaRepository<Scope, Long> {
+    Optional<Scope> findByName(String name);
 }
