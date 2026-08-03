dc47de848f9b5d5786225be278bfc19d05ac2a97
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-08-02 21:29:39 -0300

    feat: add isPlatformAdmin method as jwtUtil

--- src/main/java/enzosdev/bjjtrack/config/JwtUtils.java
+++ src/main/java/enzosdev/bjjtrack/config/JwtUtils.java
@@ -1,7 +1,10 @@
 package enzosdev.bjjtrack.config;
 
+import enzosdev.bjjtrack.enums.ScopeName;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class JwtUtils {
@@ -12,6 +15,11 @@ public class JwtUtils {
 
     public Long getAcademyIdToken(Jwt jwt){
         return jwt.getClaim("academy_id");
+    }
+
+    public boolean isPlatformAdmin(Jwt jwt){
+        List<String> scopes = jwt.getClaim("scope");
+        return scopes != null && scopes.contains(ScopeName.PLATFORM_ADMIN.getValue());
     }
 
 }
